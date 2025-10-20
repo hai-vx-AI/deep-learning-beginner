a1f70f655e42126bc6d91ee19765b65f9bea45fb
Hai Vu <vuxuanhaitdvbhp47@gmail.com>
2025-10-20 09:30:26 +0700

    gg_colab

---
+++ gg_colab.py
@@ -0,0 +1,6 @@
+!pip install torch torchvision scikit-learn
+!pip install gdown
+!pip install opencv-python
+!gdown --id 1y1b4XF0kiEK-y6nphNEi7e_4e0UB5weP
+!unzip -oq *.zip -d /content/cifar10
+!pip install tqdm
